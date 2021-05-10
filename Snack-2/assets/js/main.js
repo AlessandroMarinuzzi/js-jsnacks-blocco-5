3443e14615668430bcc54500ce02a47411da9ea5
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-10 14:43:03 +0100

    - Complete JSnack 2

--- Snack-2/assets/js/main.js
+++ Snack-2/assets/js/main.js
@@ -5,15 +5,41 @@
 
 // Create 10 courgette objects.
 var zucchiniArray = [
-    { length:12},
-    { length:11},
-    { length:15},
-    { length:14},
-    { length:10},
-    { length:11},
-    { length:11},
-    { length:13},
-    { length:14},
-    { length:11}
+    { weight:10, length:12},
+    { weight:12, length:18},
+    { weight:11, length:15},
+    { weight:10, length:14},
+    { weight:9, length:17},
+    { weight:13, length:11},
+    { weight:8, length:19},
+    { weight:7, length:13},
+    { weight:12, length:16},
+    { weight:10, length:11}
 ];
 console.log(zucchiniArray);
+
+// Split them in 2 different arrays, 1 for zucchini from 15 cm up and 1 for those under 15 cm.
+var longZucchini = [];
+var shortZucchini = [];
+
+for(var i = 0; i < zucchiniArray.length; i++){
+    if (zucchiniArray[i].length >= 15){
+        longZucchini.push(zucchiniArray[i])
+    } else {
+        shortZucchini.push(zucchiniArray[i])
+    }
+}
+console.log(longZucchini);
+console.log(shortZucchini);
+
+// Calculate the final weight of both arrays.
+var shortZucchiniWeight = 0;
+var longZucchiniWeight = 0;
+
+for(var i = 0; i < longZucchini.length , i < shortZucchini.length; i++){
+    shortZucchiniWeight += shortZucchini[i].weight;
+    longZucchiniWeight += longZucchini[i].weight;
+
+}
+console.log(shortZucchiniWeight);
+console.log(longZucchiniWeight);
