72fd6240f3c4e482c36487a83be60f1ee3b0dec4
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-05-10 13:13:41 +0100

    - Create array containing 10 objects. - Establish weight and lenght for each object. - Calculate total weight.

--- Snack-1/assets/js/main.js
+++ Snack-1/assets/js/main.js
@@ -1,3 +1,25 @@
 // Snack 1:
-// Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
-// Calcola quanto pesano tutte le zucchine.
+// Create 10 objects "zucchini" within an array and give a weight and length to each of them.
+
+var zucchiniArray = [
+    {weight:10, length:12},
+    {weight:12, length:11},
+    {weight:11, length:15},
+    {weight:10, length:14},
+    {weight:9, length:10},
+    {weight:13, length:11},
+    {weight:8, length:11},
+    {weight:7, length:13},
+    {weight:12, length:14},
+    {weight:10, length:11}
+];
+console.log(zucchiniArray);
+
+// Calculate zucchini total weight.
+var weightSum = 0;
+for(var i=0;i<zucchiniArray.length;i++){
+    var courgette = zucchiniArray[i];
+    var courgetteWeight = courgette.weight
+    weightSum += courgetteWeight; 
+}
+console.log(weightSum);
